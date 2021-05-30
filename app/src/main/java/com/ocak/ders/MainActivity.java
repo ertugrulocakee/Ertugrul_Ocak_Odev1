af769e7085135945447ef2922f7580148079c7c7
ertugrulocakee <ertugrulocakee@gmail.com>
2021-05-30 20:18:23 +0300

    ertugrul

--- app/src/main/java/com/ocak/ders/MainActivity.java
+++ app/src/main/java/com/ocak/ders/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int count = 0;
 
-    EditText kullaniciAdiText, kullaniciSifreText;
+    EditText kullaniciEmailText, kullaniciSifreText;
     Button buttonGiris , buttonKayit;
 
 
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        kullaniciAdiText= findViewById(R.id.kullaniciGirisText);
+        kullaniciEmailText= findViewById(R.id.kullaniciEmailGirisText);
         kullaniciSifreText = findViewById(R.id.kullaniciSifreText);
         buttonGiris = findViewById(R.id.buttonGiris);
         buttonKayit = findViewById(R.id.buttonKayit);
@@ -36,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        String kullaniciAdi = kullaniciAdiText.getText().toString();
+        String kullaniciEmail = kullaniciEmailText.getText().toString();
         String kullaniciSifre = kullaniciSifreText.getText().toString();
 
-        if (!kullaniciAdi.equals("") && !kullaniciSifre.equals("")) {
+        if (!kullaniciEmail.equals("") && !kullaniciSifre.equals("")) {
 
 
             try{
@@ -48,40 +48,44 @@ public class MainActivity extends AppCompatActivity {
 
                 int kullaniciAdiIndex = cursor.getColumnIndex("isim");
                 int kullaniciSifreIndex = cursor.getColumnIndex("sifre");
-
+                int kullaniciEmailIndex = cursor.getColumnIndex("email");
 
                 while (cursor.moveToNext()){
 
 
-                    String kullanici = cursor.getString(kullaniciAdiIndex);
+
                     String sifre = cursor.getString(kullaniciSifreIndex);
+                    String email = cursor.getString(kullaniciEmailIndex);
+                    String kullanici = cursor.getString(kullaniciAdiIndex);
 
 
 
 
-                    if(kullaniciAdi.equals(kullanici) && kullaniciSifre.equals(sifre)){
+                    if(kullaniciEmail.equals(email) && kullaniciSifre.equals(sifre)){
 
-                         Toast.makeText(this,"Hoşgeldiniz"+" "+kullaniciAdi, Toast.LENGTH_LONG).show();
+                         Toast.makeText(this,"Hoşgeldiniz"+" "+kullanici, Toast.LENGTH_LONG).show();
                          Intent intent = new Intent(this,MenuActivity.class);
                          startActivity(intent);
                          finish();
 
-                    }else{
-                        Toast.makeText(this,"Böyle bir hesap kaydı yok ! Lütfen tekrar deneyiniz!",Toast.LENGTH_SHORT).show();
-                        count++;
-                        if (count == 3) {
-
-                            buttonGiris.setVisibility(View.GONE);
-                            Toast.makeText(this,"3 defa yanlış giriş yaptınız!",Toast.LENGTH_SHORT).show();
-                            finish();
-
-                        }
 
                     }
 
                 }
 
                 cursor.close();
+
+                Toast.makeText(this,"Böyle bir hesap kaydı yok ! Lütfen tekrar deneyiniz!",Toast.LENGTH_SHORT).show();
+                count++;
+                if (count == 3) {
+
+                    buttonGiris.setVisibility(View.GONE);
+                    Toast.makeText(this,"3 defa yanlış giriş yaptınız!",Toast.LENGTH_SHORT).show();
+                    finish();
+
+                }
+
+
 
             }catch (Exception e){
 
